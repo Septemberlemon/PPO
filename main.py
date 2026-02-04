a95531149044ac8765180cf3e080ac11e6806907
septemberlemon <3206709613@qq.com>
2026-02-04 15:11:21 +0800

    update

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from models import Actor, Critic
 
 
 test_name = "PPO"
-episodes = 100
 gamma = 0.99
 actor_learning_rate = 0.0005
 critic_learning_rate = 0.001
+iterations = 10
 importance_sampling_size = 100
 k_epochs = 10
 
@@ -22,8 +22,8 @@ critic = Critic(env.observation_space.shape[0]).to("cuda")
 actor_optimizer = torch.optim.Adam(actor.parameters(), lr=actor_learning_rate)
 critic_optimizer = torch.optim.Adam(critic.parameters(), lr=critic_learning_rate)
 
+wandb.init(project="LunarLander-v3", name=test_name)
 
-# wandb.init(project="LunarLander-v3", name=test_name)
 
 def sample():
     trajectories = []
@@ -37,17 +37,35 @@ def sample():
             with torch.no_grad():
                 obs_tensor = torch.from_numpy(obs).to("cuda")
                 logits = actor(obs_tensor)
-                dist = Categorical(logits)
+                dist = Categorical(logits=logits)
                 action = dist.sample()
-                obs, reward, terminated, truncated, done = env.step(action.item())
+                obs, reward, terminated, truncated, info = env.step(action.item())
                 observations.append(obs)
                 log_probs.append(dist.log_prob(action))
                 actions.append(action)
                 rewards.append(reward)
 
-            if done:
+            if terminated or truncated:
                 break
-        trajectories.append((observations, log_probs, actions, rewards, terminated))
+
+        GAEs = []
+        GAE = torch.tensor(0.).to("cuda")
+        for i, reward in enumerate(rewards[::-1]):
+            td_target = torch.tensor(reward, dtype=torch.float32).to("cuda")
+            if i != 0 or not terminated:
+                td_target += gamma * critic(torch.from_numpy(observations[len(log_probs) - i]).to("cuda"))
+            V = critic(torch.from_numpy(observations[len(log_probs) - 1 - i]).to("cuda"))
+            td_error = td_target - V
+            GAE *= 0.95 * gamma
+            GAE += td_error.detach()
+            GAEs.append(GAE)
+        trajectories.append((observations, log_probs, actions, rewards, GAEs, terminated))
+        print(f"steps: {len(actions)}")
+        print(f"reward: {sum(rewards)}")
+        wandb.log({
+            "steps": len(actions),
+            "reward": sum(rewards),
+        })
     return trajectories
 
 
@@ -58,27 +76,30 @@ def learn(trajectories):
         total_critic_loss = torch.tensor(0.).to("cuda")
         total_actor_loss = torch.tensor(0.).to("cuda")
         for trajectory in trajectories:
-            observations, log_probs, actions, rewards, terminated = trajectory
-            GAE = torch.tensor(0.).to("cuda")
-            for i, log_prob, action, reward in enumerate(zip(log_probs[::-1], actions[::-1], rewards[::-1])):
+            observations, log_probs, actions, rewards, GAEs, terminated = trajectory
+            for i, (log_prob, action, reward, GAE) in enumerate(
+                    zip(log_probs[::-1], actions[::-1], rewards[::-1], GAEs)):
                 td_target = torch.tensor(reward, dtype=torch.float32).to("cuda")
                 j = len(log_probs) - 1 - i
                 if i != 0 or not terminated:
-                    td_target += gamma * critic(torch.from_numpy(observations[j]).to("cuda"))
-                V = critic(torch.from_numpy(observations[j + 1]).to("cuda"))
-                td_error = td_target - V
-                GAE *= 0.95 * gamma
-                GAE += td_error.detach()
+                    td_target += gamma * critic(torch.from_numpy(observations[j + 1]).to("cuda"))
+                V = critic(torch.from_numpy(observations[j]).to("cuda"))
 
-                logits = actor(torch.from_numpy(observations[j + 1]).to("cuda"))
-                dist = Categorical(logits)
-                ratio = torch.exp(dist.log_prob(action) - log_prob)
+                logits = actor(torch.from_numpy(observations[j]).to("cuda"))
+                dist = Categorical(logits=logits)
+                ratio = torch.exp(dist.log_prob(action) - log_prob.detach())
 
-                critic_loss = torch.functional.F.mse_loss(td_target.detach(), V)
+                critic_loss = torch.functional.F.mse_loss(V, td_target.detach())
                 actor_loss = - torch.min(ratio * GAE, torch.clamp(ratio, 1 - 0.2, 1 + 0.2) * GAE)
                 total_critic_loss += critic_loss
                 total_actor_loss += actor_loss
 
+        print(f"critic loss: {total_critic_loss.item()}")
+        print(f"actor loss: {total_actor_loss.item()}")
+        wandb.log({
+            "critic loss": total_critic_loss.item(),
+            "actor loss": total_actor_loss.item(),
+        })
         total_critic_loss.backward()
         total_actor_loss.backward()
         critic_optimizer.step()
@@ -86,6 +107,10 @@ def learn(trajectories):
 
 
 if __name__ == "__main__":
-    for episode in range(episodes):
+    for iteration in range(iterations):
+        print(f"Iteration: {iteration}")
         trajectories = sample()
         learn(trajectories)
+
+    torch.save(actor.state_dict(), f"checkpoints/{test_name}.pth")
+    torch.save(critic.state_dict(), f"checkpoints/{test_name}.pth")
