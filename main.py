facd5e73526cd00d983e0a3a1b61aab06a14a785
septemberlemon <3206709613@qq.com>
2026-02-09 16:24:36 +0800

    update

--- main.py
+++ main.py
@@ -23,8 +23,10 @@ clip_range = 0.2
 
 env = gym.make(env_name)
 
-actor = Actor(env.observation_space.shape[0], env.action_space.n).to("cuda")
-critic = Critic(env.observation_space.shape[0]).to("cuda")
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.n
+actor = Actor(state_dim, action_dim).to("cuda")
+critic = Critic(state_dim).to("cuda")
 
 actor_optimizer = torch.optim.Adam(actor.parameters(), lr=actor_learning_rate)
 critic_optimizer = torch.optim.Adam(critic.parameters(), lr=critic_learning_rate)
@@ -64,25 +66,26 @@ def rollout():
             actions = torch.stack(actions)
             rewards = torch.tensor(rewards, dtype=torch.float32).to("cuda")
 
-            GAEs = []
+            gaes = []
             returns = []
-            GAE = torch.zeros([], device="cuda")
-            V_s = critic(observations)
-            for i in range(actions.shape[0] - 1, -1, -1):
+            gae = torch.zeros([], device="cuda")
+            values = critic(observations)
+            trajectory_length = actions.shape[0]
+            for i in range(trajectory_length - 1, -1, -1):
                 td_target = rewards[i].clone()
-                if i != actions.shape[0] - 1 or truncated:
-                    td_target += gamma * V_s[i + 1]
-                GAE *= gae_lambda * gamma
-                GAE += td_target
-                returns.append(GAE.clone())
-                GAE -= V_s[i]
-                GAEs.append(GAE.clone())
-            GAEs.reverse()
-            GAEs = torch.stack(GAEs)
+                if i != trajectory_length - 1 or truncated:
+                    td_target += gamma * values[i + 1]
+                gae *= gae_lambda * gamma
+                gae += td_target
+                returns.append(gae.clone())
+                gae = gae - values[i]
+                gaes.append(gae.clone())
+            gaes.reverse()
+            gaes = torch.stack(gaes)
             returns.reverse()
             returns = torch.stack(returns)
         observations = observations[:-1]
-        trajectories.append((observations, log_probs, actions, returns, GAEs))
+        trajectories.append((observations, log_probs, actions, returns, gaes))
         print(f"steps: {actions.shape[0]}")
         print(f"reward: {rewards.sum().item()}")
         wandb.log({
@@ -93,10 +96,10 @@ def rollout():
 
 
 def learn(trajectories, entropy_coef):
-    observations, log_probs, actions, returns, GAEs = (torch.cat(data) for data in zip(*trajectories))
-    GAEs = (GAEs - GAEs.mean()) / (GAEs.std() + 1e-8)
+    observations, log_probs, actions, returns, gaes = (torch.cat(data) for data in zip(*trajectories))
+    gaes = (gaes - gaes.mean()) / (gaes.std() + 1e-8)
     for epoch in range(n_epochs):
-        sampler = BatchSampler(SubsetRandomSampler(range(actions.shape[0])), batch_size, True)
+        sampler = BatchSampler(SubsetRandomSampler(range(actions.shape[0])), batch_size, False)
         total_critic_loss = 0
         total_actor_loss = 0
         for indices in sampler:
@@ -104,15 +107,14 @@ def learn(trajectories, entropy_coef):
             mb_log_probs = log_probs[indices]
             mb_actions = actions[indices]
             mb_returns = returns[indices]
-            mb_GAEs = GAEs[indices]
+            mb_gaes = gaes[indices]
 
             critic_loss = F.mse_loss(critic(mb_observations), mb_returns)
 
             logits = actor(mb_observations)
             dist = Categorical(logits=logits)
             ratio = torch.exp(dist.log_prob(mb_actions) - mb_log_probs)
-            actor_loss = - torch.mean(
-                torch.min(ratio * mb_GAEs, torch.clamp(ratio, 1.0 - clip_range, 1.0 + clip_range) * mb_GAEs))
+            actor_loss = - torch.mean(torch.min(ratio * mb_gaes, torch.clamp(ratio, 1.0 - clip_range, 1.0 + clip_range) * mb_gaes))
 
             total_critic_loss += critic_loss.item()
             total_actor_loss += actor_loss.item()
@@ -139,7 +141,7 @@ if __name__ == "__main__":
     for iteration in range(iterations):
         print(f"Iteration: {iteration}")
         trajectories = rollout()
-        learn(trajectories, entropy_coef=0.01 * (1 - iteration / iterations))
+        learn(trajectories, entropy_coef=0.01 * (1 - iteration / iterations) * 0)
 
     env.close()
     wandb.finish()
